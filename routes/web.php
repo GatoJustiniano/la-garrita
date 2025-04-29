3cc709792f343314a54f1f6a1dc46efa1449757d
GatoJustiniano <andres95liverpool@gmail.com>
2025-04-29 03:34:30 +0000

    mejoras, composer, package, logs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
+use Rap2hpoutre\LaravelLogViewer\LogViewerController;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+    
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('logs', [LogViewerController::class, 'index'])->name('logs');
+});
+
+
